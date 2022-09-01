44326888d7a282af023ec1337547a0c71edd4e8e
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-09-01 20:36:09 +0600

    configured setup

--- pyvdo/__main__.py
+++ pyvdo/__main__.py
@@ -1,9 +1,30 @@
-from pyvdo.playlist import downloadPlaylist
+#!/usr/bin/env python3
+
+import sys
+import click
+from .__version__ import __version__
+from .playlist import downloadPlaylist
 
 
+@click.group()
+@click.version_option(__version__)
 def main():
-    downloadPlaylist()
+    """A pyvdo Download and Lookup CLI"""
+    pass
 
 
-if __name__ == "__main__":
+@main.command()
+@click.argument('playlist', required=False)
+def playlist(**kwargs):
+    """Search through CVE Database for vulnerabilities"""
+    if not kwargs['playlist']:
+        click.echo("Invalid positional argument: no playlist url was provided")
+    else:
+        downloadPlaylist(kwargs['playlist'])
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if "--help" in args or len(args) == 1:
+        print("PYVDO")
     main()
--- pyvdo/playlist.py
+++ pyvdo/playlist.py
@@ -1,5 +1,6 @@
 from pytube import Playlist, StreamQuery, Stream
 from pytube.helpers import safe_filename
+from click import echo
 
 
 res = ["720p", "480p", "360p", "240p", "144p"]
@@ -15,16 +16,15 @@ def getHighResolutionVideo(streams: StreamQuery) -> Stream:
 #
 
 
-def downloadPlaylist():
-    pin = input("Enter playlist url: ")
-    p = Playlist(pin)
-    print(f'Downloading: {p.title}')
+def downloadPlaylist(playlist: str):
+    p = Playlist(playlist)
+    echo(f'Downloading: {p.title}')
 
     for index, video in enumerate(p.videos):
         st = getHighResolutionVideo(video.streams)
 
         if st:
             filename = f'{index+1}. {safe_filename(st.title)} ({st.resolution}).mp4'
-            print(filename)
+            echo(filename)
             st.download(output_path=safe_filename(p.title), filename=filename)
             # video.streams.first().download()
